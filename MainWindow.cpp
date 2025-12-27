ecdea72c8ac4fcff49cd5b985f0532a2e2c2ac25
WhangEli <1069760166@qq.com>
2025-12-27 11:03:48 +0800

    added a chart and rearranged

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -12,6 +12,8 @@ MainWindow::MainWindow(QWidget* parent)
     InitSerialPort();
 
     InitSendDataBtn();
+
+    InitChart();
 }
 
 void MainWindow::InitMainWindow()
@@ -19,9 +21,12 @@ void MainWindow::InitMainWindow()
     setWindowTitle("串口数据接收显示");
 
     mainLayout = new QVBoxLayout(ui->centralwidget);
-    mainLayout->addStretch(2);
-    mainLayout->addWidget(ui->dataLabel, 0, Qt::AlignCenter);
-    mainLayout->addStretch(1);
+
+    topLayout = new QHBoxLayout();
+    topLayout->addWidget(ui->dataLabel);
+    topLayout->addStretch();
+
+    mainLayout->addLayout(topLayout);
 }
 
 void MainWindow::SendData()
@@ -45,6 +50,18 @@ void MainWindow::UpdateData()
     if (!receivedData.isEmpty()) {
         ui -> dataLabel -> setText(QString("接收的数据：%1").arg(receivedData));
         qDebug() << "接收到串口数据：" << receivedData;
+
+        for (int i = 0; i < data.size(); ++i) {
+            unsigned char byte = data[i];
+            qint64 timestamp = QDateTime::currentMSecsSinceEpoch();
+            dataPoints.append(QPointF(timestamp, byte));
+
+            if (dataPoints.size() > 10) {
+                dataPoints.removeFirst();
+            }
+        }
+
+        UpdateChart();
     }
 }
 
@@ -73,10 +90,62 @@ void MainWindow::InitSerialPort()
 void MainWindow::InitSendDataBtn()
 {
     QPushButton* sendDataBtn = new QPushButton("发送", this);
-    mainLayout->addWidget(sendDataBtn, 0, Qt::AlignCenter);
-    mainLayout->addStretch(6);
+    topLayout->addWidget(sendDataBtn);
 
     connect(sendDataBtn, &QPushButton::clicked, this, &MainWindow::SendData);
+}
+
+void MainWindow::InitChart()
+{
+    splineSeries = new QSplineSeries();
+    splineSeries->setName("串口数据");
+
+    axisX = new QDateTimeAxis();
+    axisX->setTitleText("时间");
+    axisX->setFormat("hh:mm:ss.zzz");
+
+    axisY = new QValueAxis();
+    axisY->setTitleText("ASCII值");
+    axisY->setRange(0, 255);
+
+    QChart* chart = new QChart();
+    chart->addSeries(splineSeries);
+    chart->addAxis(axisX, Qt::AlignBottom);
+    chart->addAxis(axisY, Qt::AlignLeft);
+    splineSeries->attachAxis(axisX);
+    splineSeries->attachAxis(axisY);
+    chart->legend()->hide();
+
+    chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+    chartView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    mainLayout->addWidget(chartView);
+}
+
+void MainWindow::UpdateChart()
+{
+    splineSeries->clear();
+
+    for (const QPointF& point : dataPoints) {
+        splineSeries->append(point);
+    }
+
+    if (!dataPoints.isEmpty()) {
+        QDateTime minTime = QDateTime::fromMSecsSinceEpoch(dataPoints.first().x());
+        QDateTime maxTime = QDateTime::fromMSecsSinceEpoch(dataPoints.last().x());
+        axisX->setRange(minTime, maxTime);
+
+        double minValue = dataPoints.first().y();
+        double maxValue = dataPoints.first().y();
+        for (const QPointF& point : dataPoints) {
+            if (point.y() < minValue) minValue = point.y();
+            if (point.y() > maxValue) maxValue = point.y();
+        }
+        double padding = (maxValue - minValue) * 0.1;
+        if (padding < 1) padding = 1;
+        axisY->setRange(minValue - padding, maxValue + padding);
+    }
 }
 
 MainWindow::~MainWindow()
--- MainWindow.h
+++ MainWindow.h
@@ -7,6 +7,15 @@
 #include <QSerialPort>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QChartView>
+#include <QChart>
+#include <QSplineSeries>
+#include <QDateTimeAxis>
+#include <QValueAxis>
+#include <QVector>
+#include <QSizePolicy>
+#include <QDateTime>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -14,7 +23,17 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+QT_CHARTS_BEGIN_NAMESPACE
+    class QChartView;
+class QChart;
+class QSplineSeries;
+class QDateTimeAxis;
+class QValueAxis;
+QT_CHARTS_END_NAMESPACE
+
+        QT_CHARTS_USE_NAMESPACE
+
+    class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
@@ -27,18 +46,28 @@ public:
 
     void InitSendDataBtn();
 
+    void InitChart();
+
+    void UpdateChart();
+
     ~MainWindow();
 
 signals:
     void ReceiveData();
 
- public slots:
-     void UpdateData();
-     void SendData();
+public slots:
+    void UpdateData();
+    void SendData();
 
 private:
     Ui::MainWindow* ui;
     QSerialPort* serialPort;
     QVBoxLayout* mainLayout;
+    QHBoxLayout* topLayout;
+    QChartView* chartView;
+    QSplineSeries* splineSeries;
+    QDateTimeAxis* axisX;
+    QValueAxis* axisY;
+    QVector<QPointF> dataPoints;
 };
 #endif // MAINWINDOW_H
